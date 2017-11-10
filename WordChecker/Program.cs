c5307a800cd88c42f9232309f31ee61f7e5de4dc
Luis Chunga <luis.chunga24@gmail.com>
2017-11-09 22:56:12 -0600

    add the first funciton

---
+++ WordChecker/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = "";  // it holds the user input
+            string filename = "dict.txt";
+            List<string> wordList = new List<string>();
+
+            WordChecker Word = new WordChecker();
+
+            Word.Reding_file(filename);
+
+            Console.Write ("Enter a word:  ");
+            input = Console.ReadLine();     //read line
+
+            input = input.ToLower();
+
+            while (input != "-q")
+
+            {
+
+                if (Word.SearchWord(input))
+                {
+                    Console.WriteLine("OK");
+                }
+                else {
+
+                    wordList = Word.Word_Space(input); 
+                   
+
+                }
+
+                Display(wordList);
+                                     
+                Console.Write("Enter a word:  ");
+                input = Console.ReadLine();
+                input = input.ToLower();
+            }
+
+
+            Console.WriteLine("Aplication ends");
+            Console.ReadKey();
+        }
+
+
+        public static void  Display(List <string> list) {
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+
+    }
+}
---
+++ WordChecker/WordChecker.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;    // use to read file
+using System.Collections;
+
+namespace ConsoleApplication2
+{
+    class WordChecker
+    {
+        Hashtable dictionary = new Hashtable();
+        List<string> listWord = new List<string>();
+
+        public void Reding_file(string filename) {
+
+            string word = "";
+            // opens a external file
+            StreamReader inFile = new StreamReader(@filename);
+       
+            while((word = inFile.ReadLine()) != null)
+                dictionary.Add(word, word);
+            
+
+            inFile.Close();
+        }
+        
+           
+        // this function return true if the word is in the list 
+        public bool SearchWord (string words) {
+
+            return dictionary.ContainsValue(words);           
+              
+        }
+
+
+        public List<string> Word_Space(string word) {
+
+            string s1 = "";
+            string s2 = "";
+
+            for (int i = 1; i < word.Length; i++)
+            {
+                s1 = word.Substring(0, i);
+                s2 = word.Substring(i, word.Length-i);
+
+                if (SearchWord(s1) && SearchWord(s2))
+                    listWord.Add(s1 + " " +s2);
+                   
+                
+
+            }
+
+            return listWord;
+        }
+
+
+
+
+    }
+}
